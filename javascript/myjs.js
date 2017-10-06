109add92fed8e6f37dbe78842dfa6145a43ccee7
anukritiabbott08 <anukriti.abbott1@marist.edu>
2017-10-06 10:51:43 -0400

    Add files via upload

---
+++ javascript/myjs.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+	/*Function to open index page on home tab*/
+	$( "#home" ).click(function() {
+		document.location.href="index.html";
+	});
+	
+	/*Function to open resume page on resume tab*/
+	$( "#resume" ).click(function() {
+		document.location.href="resume.html";
+	});
+	
+	/*Function to open ContactUs page on Contact Me tab*/
+	$( "#contact" ).click(function() {
+		document.location.href="contactUs.html";
+	});
+});
